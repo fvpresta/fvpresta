9ab6e2fbe5d51bc02f0c4337427c7190804da47a
hthieu1110 <fvpresta@gmail.com>
2012-11-11 22:59:41 +0100

    Fix config dev

--- settings/dev.py
+++ settings/dev.py
@@ -6,14 +6,14 @@ from settings.base import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-PROJECT_ROOT_PATH = '/home/hthieu/www/pro/django-projects/fvpresta'
+PROJECT_ROOT_PATH = 'D:/www/Django-Projects/fvpresta';
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'hthieu_fvpresta',                      # Or path to database file if using sqlite3.
-        'USER': 'hthieu_fvpresta',                      # Not used with sqlite3.
-        'PASSWORD': 'fvpresta',                  # Not used with sqlite3.
+        'NAME': 'fvpresta',                      # Or path to database file if using sqlite3.
+        'USER': 'fvpresta',                      # Not used with sqlite3.
+        'PASSWORD': 'hahaha',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
